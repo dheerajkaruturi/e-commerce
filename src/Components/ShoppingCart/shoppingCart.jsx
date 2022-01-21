fa2de35d45ba088d5b4310b6a365bb07d492523c
dheeraj karuturi <dheeraj.karuturi@audreeinfotech.com>
2022-01-21 14:55:14 +0530

    number of items in cart and some conditional stmt

--- src/Components/ShoppingCart/shoppingCart.jsx
+++ src/Components/ShoppingCart/shoppingCart.jsx
@@ -1,18 +1,35 @@
-import { Fragment, useContext } from "react";
+import { Fragment, useContext, useState, useEffect } from "react";
 import "./shoppingCart.css";
 
 import CartContext from "../store/CartContext";
 
 const ShoppingCart = () => {
+  const [cartList, setcartList] = useState(false);
   const cartItemList = useContext(CartContext);
   const displayCart = cartItemList.items;
-  console.log(displayCart)
+
+  useEffect(() => {
+    if (cartItemList.items.length !== 0) {
+      setcartList(true);
+    }
+  }, [cartItemList]);
+
+  console.log(displayCart);
   return (
     <Fragment>
       <section className="cart-section">
         <h3>
           Shopping Cart <i className="icon ion-md-cart"></i>
         </h3>
+        <p
+          style={{
+            fontSize: "0.75rem",
+            fontWeight: "bold",
+            marginTop: "0.5rem",
+          }}
+        >
+          There are {`${displayCart.length}`} items in your cart.
+        </p>
         {displayCart.map((cartItem) => (
           <div className="cartitemsList-container">
             <div className="item_img">
@@ -30,9 +47,15 @@ const ShoppingCart = () => {
           </div>
         ))}
         {/* render this bill part conditionally */}
-        {/* <div className="bill_summary">
-          <h4>Total Amount : 6000 ₹</h4>
-        </div> */}
+        {cartList ? (
+          <div className="bill_summary">
+            <h4>Total Amount : 6000 ₹</h4>
+          </div>
+        ) : (
+          <div className="no_cartItems">
+            <p>No items to show... 🤷🏼‍♂️</p>
+          </div>
+        )}
       </section>
     </Fragment>
   );
